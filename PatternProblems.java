994529d861c48cf0d0e3b4d849b8f3acd1571630
Aswin Balasubramanian <b.aswin32@gmail.com>
2026-04-12 20:05:35 +0530

    Initial commit with PatternProblems.java

---
+++ PatternProblems.java
@@ -0,0 +1,67 @@
+import java.util.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i = 1; i <= n; i++){
+		    int input = sc.nextInt();
+		    print18(input);
+		}
+        sc.close();
+	}
+	
+	static void print13( int n ){
+	    int print = 1;
+	    for( int i = 1; i <= n; i++ ){
+	        
+    	    for( int j = 1; j <= i; j++ ){
+    	        System.out.print( print +" ");
+    	        print++;
+    	    }
+    	    System.out.println(); 
+	    }
+	}
+	
+	static void print17( int n ){
+	    for( int i = 0; i < n; i++){
+	        for( int j = 0; j < n - i - 1; j++){
+	            System.out.print(" ");
+	        }
+	        
+	        char ch = 'A';
+	        int breakpoint = (2*i + 1)/2;
+	        for( int j = 1; j <= 2*i+1; j++ ){
+	            System.out.print( ch );
+	            if( j <= breakpoint) 
+	                ch++;
+	            else 
+	                ch--;
+	        }
+	        
+	        for( int j = 0; j < n - i - 1; j++ ){
+	            System.out.print(" ");
+	        }
+	        System.out.println();
+	    }
+	}
+	
+	static void print18( int n ){
+	    for( int i = 1; i <= n; i++ ){
+	        char ch = 'E';
+	        ch -= i;
+	        ch += 1;
+	        for( int j = 1; j <= i; j++ ){
+	            System.out.print(ch); ch++;
+	        }
+	        for( int j = 1; j <= n - i; j++){
+	            System.out.print(" ");
+	        }
+	        System.out.println();
+	    }
+	    
+	}
+}
